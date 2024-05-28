b7038ae1655fde3b89c2eaedcae3083642944ed9
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-05-28 12:40:02 +0530

    Updated the code

--- server.js
+++ server.js
@@ -48,38 +48,39 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const express = require('express');
-const { PrismaClient } = require('@prisma/client');
-const dotenv=require('dotenv')
-dotenv.config()
-const authRouter=require('./routes/authRouter')
-const attendanceRouter=require('./routes/attendanceRouter')
-const employeeRouter=require('./routes/employeeRouter')
-const cors=require('cors')
-const cookieParser=require('cookie-parser')
-// @initializing prisma and express app
-const prisma = new PrismaClient();
-const corsOptions = {
-  origin:' http://localhost:5173',
-  // origin:' https://qubinest-frontend.vercel.app',
-  credentials: true, // This is required to allow credentials (cookies, headers)
-};
-//@middlewares
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use('/qubinest',authRouter)
-app.use('/qubinest',attendanceRouter)
-app.use('/qubinest',employeeRouter)
-app.use(cookieParser())
+// const express = require('express');
+// const { PrismaClient } = require('@prisma/client');
+// const dotenv=require('dotenv')
+// dotenv.config()
+// const authRouter=require('./routes/authRouter')
+// const attendanceRouter=require('./routes/attendanceRouter')
+// const employeeRouter=require('./routes/employeeRouter')
+// const cors=require('cors')
+// const cookieParser=require('cookie-parser')
+// // @initializing prisma and express app
+// const prisma = new PrismaClient();
+// const app = express();
+// const corsOptions = {
+//   origin:' http://localhost:5173',
+//   // origin:' https://qubinest-frontend.vercel.app',
+//   credentials: true, // This is required to allow credentials (cookies, headers)
+// };
+// //@middlewares
+// app.use(cors(corsOptions))
+// app.use(express.json())
+// app.use('/qubinest',authRouter)
+// app.use('/qubinest',attendanceRouter)
+// app.use('/qubinest',employeeRouter)
+// app.use(cookieParser())
 
-// @prisma config
-async function shutdown() {
-  await prisma.$disconnect();
-  process.exit(0);
-}
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
-const PORT = process.env.PORT ;
+// // @prisma config
+// async function shutdown() {
+//   await prisma.$disconnect();
+//   process.exit(0);
+// }
+// process.on('SIGINT', shutdown);
+// process.on('SIGTERM', shutdown);
+// const PORT = process.env.PORT ;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
