ed1e308ab2bc20df35b171cc74ccf0b17881c1c4
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-05-28 12:46:00 +0530

    Test

--- server.js
+++ server.js
@@ -1,92 +1,45 @@
-// const express = require('express');
-// const { PrismaClient } = require('@prisma/client');
-// const dotenv=require('dotenv')
-// dotenv.config()
-// const authRouter=require('./routes/authRouter')
-// const attendanceRouter=require('./routes/attendanceRouter')
-// const employeeRouter=require('./routes/employeeRouter')
-// const cors=require('cors')
-// const cookieParser=require('cookie-parser')
-// // @initializing prisma and express app
-// const prisma = new PrismaClient();
-// const app = express();
-// const corsOptions = {
-//   origin:' http://localhost:5173',
-//   // origin:' https://qubinest-frontend.vercel.app',
-//   credentials: true, // This is required to allow credentials (cookies, headers)
-// };
-// //@middlewares
-// app.use(cors(corsOptions))
-// app.use(express.json())
-// app.use('/qubinest',authRouter)
-// app.use('/qubinest',attendanceRouter)
-// app.use('/qubinest',employeeRouter)
-// app.use(cookieParser())
-
-// // @prisma config
-// async function shutdown() {
-//   await prisma.$disconnect();
-//   process.exit(0);
-// }
-// process.on('SIGINT', shutdown);
-// process.on('SIGTERM', shutdown);
-// const PORT = process.env.PORT ;
-
-// // @starting app
-// app.get("/",(req,res)=>{
-//   res.send("Api is working fine")
-// })
-// app.get("/test",(req,res)=>{
-//   res.send("This is a test")
-// })
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const dotenv=require('dotenv')
+dotenv.config()
+const authRouter=require('./routes/authRouter')
+const attendanceRouter=require('./routes/attendanceRouter')
+const employeeRouter=require('./routes/employeeRouter')
+const cors=require('cors')
+const cookieParser=require('cookie-parser')
+// @initializing prisma and express app
+const prisma = new PrismaClient();
 const app = express();
-const port = 3000;
+const corsOptions = {
+  // origin:' http://localhost:5173',
+  origin:' https://qubinest-frontend.vercel.app',
+  credentials: true, // This is required to allow credentials (cookies, headers)
+};
+//@middlewares
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use('/qubinest',authRouter)
+app.use('/qubinest',attendanceRouter)
+app.use('/qubinest',employeeRouter)
+app.use(cookieParser())
 
-// const express = require('express');
-// const { PrismaClient } = require('@prisma/client');
-// const dotenv=require('dotenv')
-// dotenv.config()
-// const authRouter=require('./routes/authRouter')
-// const attendanceRouter=require('./routes/attendanceRouter')
-// const employeeRouter=require('./routes/employeeRouter')
-// const cors=require('cors')
-// const cookieParser=require('cookie-parser')
-// // @initializing prisma and express app
-// const prisma = new PrismaClient();
-// const app = express();
-// const corsOptions = {
-//   origin:' http://localhost:5173',
-//   // origin:' https://qubinest-frontend.vercel.app',
-//   credentials: true, // This is required to allow credentials (cookies, headers)
-// };
-// //@middlewares
-// app.use(cors(corsOptions))
-// app.use(express.json())
-// app.use('/qubinest',authRouter)
-// app.use('/qubinest',attendanceRouter)
-// app.use('/qubinest',employeeRouter)
-// app.use(cookieParser())
+// @prisma config
+async function shutdown() {
+  await prisma.$disconnect();
+  process.exit(0);
+}
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+const PORT = process.env.PORT ;
 
-// // @prisma config
-// async function shutdown() {
-//   await prisma.$disconnect();
-//   process.exit(0);
-// }
-// process.on('SIGINT', shutdown);
-// process.on('SIGTERM', shutdown);
-// const PORT = process.env.PORT ;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// @starting app
+app.get("/",(req,res)=>{
+  res.send("Api is working fine")
+})
+app.get("/test",(req,res)=>{
+  res.send("This is a test")
+})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
