bba33800309673b8cd1bce78eaf107c88a16121d
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-05-28 13:37:13 +0530

    test

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
 const express = require('express');
-// const { PrismaClient } = require('@prisma/client');
-// const dotenv=require('dotenv')
-// dotenv.config()
-const authRouter=require('./routes/authRouter')
-const attendanceRouter=require('./routes/attendanceRouter')
-const employeeRouter=require('./routes/employeeRouter')
-// const cors=require('cors')
-// const cookieParser=require('cookie-parser')
-// // @initializing prisma and express app
-// const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client');
+const dotenv = require('dotenv');
+dotenv.config();
+const authRouter = require('./routes/authRouter');
+const attendanceRouter = require('./routes/attendanceRouter');
+const employeeRouter = require('./routes/employeeRouter');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+// Initializing Prisma and Express app
+const prisma = new PrismaClient();
 const app = express();
-const PORT = 3000
-// const corsOptions = {
-//   // origin:' http://localhost:5173',
-//   origin:' https://qubinest-frontend.vercel.app',
-//   credentials: true, // This is required to allow credentials (cookies, headers)
-// };
-// // //@middlewares
-// app.use(cors(corsOptions))
-app.use(express.json())
-app.use('/qubinest',authRouter)
-app.use('/qubinest',attendanceRouter)
-app.use('/qubinest',employeeRouter)
-// app.use(cookieParser())
+const PORT = process.env.PORT || 3000;
 
-// // @prisma config
-// async function shutdown() {
-//   await prisma.$disconnect();
-//   process.exit(0);
-// }
-// process.on('SIGINT', shutdown);
-// process.on('SIGTERM', shutdown);
-// const PORT = process.env.PORT ;
+const corsOptions = {
+  origin: 'https://qubinest-frontend.vercel.app',
+  credentials: true, // This is required to allow credentials (cookies, headers)
+};
 
-// @starting app
-app.get("/",(req,res)=>{
-  res.send("Api is working fine")
-})
-app.get("/test",(req,res)=>{
-  res.send("This is a test")
-})
+// Middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/qubinest', authRouter);
+app.use('/qubinest', attendanceRouter);
+app.use('/qubinest', employeeRouter);
+app.use(cookieParser());
+
+// Prisma configuration
+async function shutdown() {
+  await prisma.$disconnect();
+  process.exit(0);
+}
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+
+// Starting app
+app.get("/", (req, res) => {
+  res.send("API is working fine");
+});
+app.get("/test", (req, res) => {
+  res.send("This is a test");
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
